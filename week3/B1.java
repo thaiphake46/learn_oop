3f442c435fd336e9398d6aaf9c2a552e4a93a75a
Huy Thai <huythaia123@gmail.com>
2022-03-13 20:55:21 +0700

    reload B1

--- week3/B1.java
+++ week3/B1.java
@@ -1,12 +1,14 @@
 public class B1 {
     public static void main(String[] args) {
-        Point A = new Point(1, -1);
-        Point B = new Point(2, 1);
-        Point C = new Point(4, 5);
+        Point A = new Point(1, -1),
+              B = new Point(2, 1),
+              C = new Point(4, 5);
 
         Point.baDiemThangHang(A, B, C);
 
-        double khoangCach = Point.KhoangCach(A, B);
-        System.out.println("- Khoang cach giua hai diem A va B: "+khoangCach);
+        
+        System.out.println("- Khoang cach giua hai diem A va B: " + Point.KhoangCach(A, B));
+        System.out.println("- Khoang cach giua hai diem A va C: " + Point.KhoangCach(A, C));
+        System.out.println("- Khoang cach giua hai diem B va C: " + Point.KhoangCach(B, C));
     }
 }
